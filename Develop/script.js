387bd16859b85e7be2d76577dcadf0e6bcdf4696
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-05-12 23:07:35 -0700

    addFunctionsToGenerateCharacters

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-var passwordLettersUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";                     //Possible password letters uppercase
-var passwordLettersLowercase = "abcdefghijklmnopqrstuvwxyz";                     //Possible password letters lowercase   
-var passwordLettersAll = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"; //All Possible password letters
-var passwordNumbers = "0123456789";                                              //Possible password numbers
-var passwordSpecialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";              //Possible password special characters. Was not able to add double quotations, need to ask about that later.
-var allPasswordCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; //All possible password characters
+// Write Password to the #password input
+// function writePassword(){
+//   var password = generatePassword;
 
-var passwordLength = prompt(
-  "How many characters do you want your password? Choose length from 8 to 128."
-);
-var passwordCharacterTypeUppercase = confirm(
-  "Press OK if you want your password to include uppercase letters."
-);
-var passwordCharacterTypeLowercase = confirm(
-  "Press OK if you want your password to include lowercase letters."
-)
-var passwordCharacterTypeNumbers = confirm(
-  "Press OK if you want your password to include numbers."
-)
-var passwordCharacterTypeSpecialCharacters = confirm(
-  "Press OK if you want your password to include special characters."
-)
+//   var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
+// };
 
 
 
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
-function writePassword(){
 
-  var passwordText = document.querySelector("#password");
+// Generator Functions
 
-  passwordText.value = password;
-
+function getRandomLowercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function getRandomUppercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol(){
+  var symbols = "!@#$%^&*()[]=<>.,/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+console.log(getRandomUppercase());
