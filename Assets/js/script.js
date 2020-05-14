a64011a40730b578a9cd474a80ac01cfaf7341fa
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-05-13 23:24:46 -0700

    addAnArrayForTheCharacterFunctions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,30 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.getElementById("generate");
+var password;
+
+// Generator Functions
+function getRandomLowercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+function getRandomUppercase(){
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+};
+function getRandomSymbol(){
+  var symbols = "!@#$%^&*()[]=<>.,/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+// Creating an object array for the character creation functions
+
+var characterArr = {
+  lowercase: getRandomLowercase,
+  uppercase: getRandomUppercase, 
+  numbers: getRandomNumber,
+  symbols: getRandomSymbol,
+};
 
 // Write Password to the #password input
 // function writePassword(){
@@ -11,29 +36,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-// Generator Functions
-
-function getRandomLowercase(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUppercase(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol(){
-  var symbols = "!@#$%^&*()[]=<>.,/";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-
-
-console.log(getRandomUppercase());
+// Add event listener to generate button
+generateBtn.addEventListener("click", function(){
+  console.log("test click!");
+});
