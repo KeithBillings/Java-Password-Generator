700ae980836fe135d2a74ffbb382c6c7d42c896f
Keith Billings <63262663+KeithBillings@users.noreply.github.com>
2020-05-15 23:30:54 -0700

    debuggingAndRefactoring

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 const generateBtn = document.getElementById("generate");
-const password = "";
+const password = document.getElementById("password");
 
 // Length input and checkboxes 
 const lengthSetting = document.getElementById("length");
@@ -10,15 +10,15 @@ const numbersSetting = document.getElementById("numbers");
 const symbolsSetting = document.getElementById("symbols");
 
 // Creating an object array for the character creation functions
-const characterFunctionObj = {
-  "lowercase": getRandomLowercase,
-  "uppercase": getRandomUppercase, 
-  "numbers": getRandomNumber,
-  "symbols": getRandomSymbol,
+const characterFunctionObj = {  
+  upper: getRandomUppercase, 
+  lower: getRandomLowercase,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
 };
 
 // When the "Generate" button is clicked, this will run
-generateBtn.addEventListener("click", () => {
+generate.addEventListener("click", () => {
   // Getting the values of the user's input of preferred settings
   const userLengthInput = +lengthSetting.value;
   const userUppercaseInput = uppercaseSetting.checked;
@@ -31,41 +31,37 @@ generateBtn.addEventListener("click", () => {
 });
 
 // Function creating the password 
-function generatePassword(lower, upper, number, symbol, length){
+function generatePassword(upper, lower, number, symbol, length){
   // Placeholder for password to manipulate
   let generatedPassword = "";
-  // Making a variable to house the different types of input settings
-  const typesCount = userUppercaseInput + userLowercaseInput + userNumbersInput + userSymbolsInput;
-
-  console.log(typesCount);
+  // TypesCount is the number of checked boxes 0-4
+  const typesCount = upper + lower + number + symbol;
   // Making an array that will filter out the unchecked boxes
-  const typesArray = [{userUppercaseInput}, {userLowercaseInput}, {userNumbersInput}, {userSymbolsInput}].filter(item => Object.values(item)[0]);
+  const typesArray = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
 
-  console.log(typesArray);
-
+  // Return Statement in text area if no setting is selected
   if(typesCount === 0){
     return "Please select an option.";
   };
-
-  for(i = 0; i < length; i += typesCount){
+  // Creating a loop that will 
+  for(let i = 0; i < length; i += typesCount){
     typesArray.forEach(type => {
       const funcName = Object.keys(type)[0];
-      console.log(funcName);
-      password += characterFunctionObj[funcName]();
-     
+      generatedPassword += characterFunctionObj[funcName]();
     });
   }
-
+  const finalPassword = generatedPassword.slice(0, length);
+  return finalPassword;
 };
 
 // Generator Functions
-function getRandomLowercase(){
-  // abcdefghijklmnopqrstuvwxyz
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
 function getRandomUppercase(){
   // ABCDEFGHIJKLMNOPQRSTUVWXYZ
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+function getRandomLowercase(){
+  // abcdefghijklmnopqrstuvwxyz
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 };
 function getRandomNumber(){
   // 0123456789
